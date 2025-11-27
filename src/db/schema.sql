9d1b63766bd0e1cbac7b2b971cd7174c3daead47
Caio Leonardo <caleobraga@gmail.com>
2025-11-27 13:21:34 -0300

    Adicionando alteração para não tentar criar tipo 'sentimento' mais de uma vez

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,6 +1,12 @@
-CREATE TYPE sentiment AS ENUM ('positive', 'negative');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'sentiment') THEN
+        CREATE TYPE sentiment AS ENUM ('positive', 'negative');
+    END IF;
+END
+$$;
 
-CREATE TABLE reviews(
+CREATE TABLE IF NOT EXISTS reviews(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     texto TEXT NOT NULL,
     label sentiment NOT NULL,
